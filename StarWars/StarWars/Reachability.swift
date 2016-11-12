282bd0a0cff4e0efcb162a80c1fd13f030c44e61
Nick Yoon <nyoon90@gmail.com>
2016-11-12 00:50:21 -0500

    Added support for reachability to check internet connection whether to make a service call

--- StarWars/StarWars/Reachability.swift
+++ StarWars/StarWars/Reachability.swift
@@ -29,3 +29,11 @@ public class Reachability {
 		return (isReachable && !needsConnection)
 	}
 }
+
+func delay(seconds: Double, completion:@escaping ()->()) {
+	let popTime = DispatchTime.now() + Double(Int64( Double(NSEC_PER_SEC) * seconds )) / Double(NSEC_PER_SEC)
+	
+	DispatchQueue.main.asyncAfter(deadline: popTime) {
+		completion()
+	}
+}
